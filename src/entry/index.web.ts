a75cb84b8794f3f7ee7224d7a53f625579bddb3a
Donatas Pocius <pocius.coding@gmail.com>
2023-08-16 00:43:57 +0300

    rnv web

--- src/entry/index.web.ts
+++ src/entry/index.web.ts
@@ -1,5 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../app';
+import React from "react";
+import ReactDOM from "react-dom";
+// import App from "../app";
+import Page from "../pages/index";
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+// ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(React.createElement(Page), document.getElementById("root"));
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
-import App from '../app';
+// import React from "react";
+// import App from "../app";
 
-const Page = () => <App />;
+// const Page = () => <App />;
+// export default Page;
+
+import React from "react";
+const Page = () => {
+  return <div>Home</div>;
+};
 export default Page;
